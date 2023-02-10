a0f8271afbbfdc6560161616263806beb25a3a46
sdmydbr9 <65818001+sdmydbr9@users.noreply.github.com>
2023-02-10 19:35:37 +0530

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -2,7 +2,6 @@ var NodeHelper = require("node_helper");
 var https = require("https");
 
 module.exports = NodeHelper.create({
-
   // Override start method.
   start: function() {
     console.log("Starting node helper for: " + this.name);
@@ -12,37 +11,46 @@ module.exports = NodeHelper.create({
   socketNotificationReceived: function(notification, payload) {
     if (notification === "GET_VIDEO_TITLES") {
       var apiKey = payload.apiKey;
-      var channelId = payload.channelId;
+      var channelIds = payload.channelId;
       var debug = payload.debug;
+      var titles = [];
 
-      var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&maxResults=50&order=date&type=video&key=${apiKey}`;
-      var self = this; // store a reference to the node helper
+      // Function to fetch the video titles for a single channel ID.
+      function fetchTitlesForChannel(channelId) {
+        var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&maxResults=50&order=date&type=video&key=${apiKey}`;
+        var self = this; // store a reference to the node helper
 
-      https.get(url, function(res) {
-        var body = "";
+        https.get(url, function(res) {
+          var body = "";
 
-        res.on("data", function(chunk) {
-          body += chunk;
+          res.on("data", function(chunk) {
+            body += chunk;
+          });
+
+          res.on("end", function() {
+            var response = JSON.parse(body);
+
+            for (var i = 0; i < response.items.length; i++) {
+              titles.push(response.items[i].snippet.title);
+            }
+
+            // Log the titles to the console if debug is enabled.
+            if (debug) {
+              console.log("Titles for channel " + channelId + ": " + titles);
+            }
+
+            // If all channels have been processed, send the video titles back to the module.
+            if (channelIds.length === 0) {
+              self.sendSocketNotification("VIDEO_TITLES", { titles: titles });
+            }
+          });
         });
+      }
 
-        res.on("end", function() {
-          var response = JSON.parse(body);
-          var titles = [];
-
-          for (var i = 0; i < response.items.length; i++) {
-            titles.push(response.items[i].snippet.title);
-          }
-
-          // Log the titles to the console if debug is enabled.
-          if (debug) {
-            console.log("Titles: " + titles);
-          }
-
-          // Send the video titles back to the module.
-          self.sendSocketNotification("VIDEO_TITLES", { titles: titles });
-        });
+      // Fetch the video titles for each channel ID.
+      channelIds.forEach(function(channelId) {
+        fetchTitlesForChannel(channelId);
       });
     }
   }
-  });
-  
+});
