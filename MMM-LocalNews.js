7bbbd0291fd9590397272190de634a322e481a04
sdmydbr9 <65818001+sdmydbr9@users.noreply.github.com>
2023-02-10 18:23:53 +0530

    Update MMM-LocalNews.js

--- MMM-LocalNews.js
+++ MMM-LocalNews.js
@@ -1,17 +1,13 @@
-Module.register("MMM-LocalNews", {
-  // Default module config.
   defaults: {
     apiKey: "",
     channelId: "",
-    updateInterval: 3600000, // Default update interval is 1 hour (in milliseconds)
     displayTime: 5000, // Default display time is 5 seconds
     debug: false // Default value for debug is false
   },
 
   // Override start method.
   start: function() {
-    this.getVideoTitles();
-    setInterval(this.getVideoTitles.bind(this), this.config.updateInterval);
+    this.sendSocketNotification("GET_VIDEO_TITLES", this.config);
   },
 
   // Handle the VIDEO_TITLES socket notification.
@@ -41,10 +37,5 @@ Module.register("MMM-LocalNews", {
     this.updateDom();
 
     setTimeout(this.showNextTitle.bind(this), this.config.displayTime);
-  },
-
-  // Get the video titles from the API.
-  getVideoTitles: function() {
-    this.sendSocketNotification("GET_VIDEO_TITLES", this.config);
   }
 });
