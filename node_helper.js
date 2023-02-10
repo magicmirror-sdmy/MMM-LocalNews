2bdd48534f155f24038b1602c441445dbcdfc472
sdmydbr9 <65818001+sdmydbr9@users.noreply.github.com>
2023-02-10 18:02:09 +0530

    Update node_helper.js
    Added cache and update interval

--- node_helper.js
+++ node_helper.js
@@ -1,8 +1,8 @@
 var NodeHelper = require("node_helper");
 var https = require("https");
+var cache = require("memory-cache");
 
 module.exports = NodeHelper.create({
-
   // Override start method.
   start: function() {
     console.log("Starting node helper for: " + this.name);
@@ -14,6 +14,17 @@ module.exports = NodeHelper.create({
       var apiKey = payload.apiKey;
       var channelId = payload.channelId;
       var debug = payload.debug;
+      var cacheKey = `videoTitles_${channelId}`;
+
+      // Check if the video titles are already cached.
+      var videoTitles = cache.get(cacheKey);
+      if (videoTitles) {
+        if (debug) {
+          console.log("Titles retrieved from cache");
+        }
+        this.sendSocketNotification("VIDEO_TITLES", { titles: videoTitles });
+        return;
+      }
 
       var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&maxResults=50&order=date&type=video&key=${apiKey}`;
       var self = this; // store a reference to the node helper
@@ -37,6 +48,9 @@ module.exports = NodeHelper.create({
           if (debug) {
             console.log("Titles: " + titles);
           }
+
+          // Store the video titles in cache for 1 hour.
+          cache.put(cacheKey, titles, 60 * 60 * 1000);
 
           // Send the video titles back to the module.
           self.sendSocketNotification("VIDEO_TITLES", { titles: titles });
