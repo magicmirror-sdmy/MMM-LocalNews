b8116de2e91c0a81801dfab0090cf6cfd032335a
sdmydbr9 <65818001+sdmydbr9@users.noreply.github.com>
2023-02-10 16:04:19 +0530

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -12,36 +12,44 @@ module.exports = NodeHelper.create({
   socketNotificationReceived: function(notification, payload) {
     if (notification === "GET_VIDEO_TITLES") {
       var apiKey = payload.apiKey;
-      var channelId = payload.channelId;
+      var urls = payload.urls;
+      var hours = payload.hours;
       var debug = payload.debug;
+      
+      var titles = [];
 
-      var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&maxResults=50&order=date&type=video&key=${apiKey}`;
-      var self = this; // store a reference to the node helper
+      urls.forEach(function(url) {
+        var channelId = url.substring(url.lastIndexOf("/") + 1);
+        var publishedAfter = new Date(Date.now() - hours * 60 * 60 * 1000).toISOString();
+        var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&publishedAfter=${publishedAfter}&maxResults=50&order=date&type=video&key=${apiKey}`;
+        var self = this; // store a reference to the node helper
 
-      https.get(url, function(res) {
-        var body = "";
+        https.get(url, function(res) {
+          var body = "";
 
-        res.on("data", function(chunk) {
-          body += chunk;
-        });
+          res.on("data", function(chunk) {
+            body += chunk;
+          });
 
-        res.on("end", function() {
-          var response = JSON.parse(body);
-          var titles = [];
+          res.on("end", function() {
+            var response = JSON.parse(body);
 
-          for (var i = 0; i < response.items.length; i++) {
-            titles.push(response.items[i].snippet.title);
-          }
-
-          // Log the titles to the console if debug is enabled.
-          if (debug) {
-            console.log("Titles: " + titles);
-          }
-
-          // Send the video titles back to the module.
-          self.sendSocketNotification("VIDEO_TITLES", { titles: titles });
+            for (var i = 0; i < response.items.length; i++) {
+              titles.push(response.items[i].snippet.title);
+            }
+          });
         });
       });
+
+      setTimeout(function() {
+        // Log the titles to the console if debug is enabled.
+        if (debug) {
+          console.log("Titles: " + titles);
+        }
+  
+        // Send the video titles back to the module.
+        self.sendSocketNotification("VIDEO_TITLES", { titles: titles });
+      }, 2000);
     }
   }
 });
