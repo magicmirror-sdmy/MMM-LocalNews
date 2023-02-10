52083b0e90d66775f241ca46c9d2f5f9b86dfb6d
sdmydbr9 <65818001+sdmydbr9@users.noreply.github.com>
2023-02-10 14:30:57 +0530

    Update MMM-LocalNews.js

--- MMM-LocalNews.js
+++ MMM-LocalNews.js
@@ -1,89 +1,36 @@
-const moment = require('moment');
+const moment = window.moment;
+
 Module.register("MMM-LocalNews", {
-    // Default module config.
-    defaults: {
-        channelId: "UCX6OQ3DkcsbYNE6H8uQQuVA",
-        apiKey: "YOUR_API_KEY",
-        updateInterval: 60 * 60 * 1000, // Every hour.
-        animationSpeed: 2 * 1000,
-        initialLoadDelay: 0,
-        retryDelay: 2500,
-        maxTitles: 10,
-        reverse: false
-    },
+  // Default module config.
+  defaults: {
+    apiKey: "",
+    channelId: ""
+  },
 
-    // Define required scripts.
-    getScripts: function() {
-        return [];
-    },
+  // Override start method.
+  start: function() {
+    this.sendSocketNotification("GET_VIDEO_TITLES", this.config);
+  },
 
-    // Define required styles.
-    getStyles: function() {
-        return [
-            "MMM-LocalNews.css"
-        ];
-    },
+  // Handle the VIDEO_TITLES socket notification.
+  socketNotificationReceived: function(notification, payload) {
+    if (notification === "VIDEO_TITLES") {
+      var titles = payload.titles;
 
-    // Define start sequence.
-    start: function() {
-        Log.info("Starting module: " + this.name);
+      var html = "<ul>";
+      for (var i = 0; i < titles.length; i++) {
+        html += "<li>" + titles[i] + "</li>";
+      }
+      html += "</ul>";
 
-        // Set locale.
-        moment.locale(config.language);
-
-        this.config = Object.assign({}, this.defaults, config);
-
-        this.videoTitles = [];
-        this.scheduleUpdate();
-    },
-
-    // Override the getDom method.
-    getDom: function() {
-        var wrapper = document.createElement("div");
-        wrapper.className = "video-titles";
-
-        if (!this.videoTitles.length) {
-            wrapper.innerHTML = "Loading video titles...";
-            wrapper.classList.add("bright", "light");
-            return wrapper;
-        }
-
-        if (this.config.reverse) {
-            this.videoTitles.reverse();
-        }
-
-        this.videoTitles.forEach(function(videoTitle) {
-            var title = document.createElement("div");
-            title.className = "video-title";
-            title.innerHTML = videoTitle;
-            wrapper.appendChild(title);
-        });
-
-        return wrapper;
-    },
-
-    // Request video titles from the node helper.
-    scheduleUpdate: function() {
-        setTimeout(function() {
-            this.sendSocketNotification("GET_VIDEO_TITLES", {
-                channelId: this.config.channelId,
-                apiKey: this.config.apiKey
-            });
-        }.bind(this), this.config.initialLoadDelay);
-
-        setInterval(function() {
-            this.sendSocketNotification("GET_VIDEO_TITLES", {
-                channelId: this.config.channelId,
-                apiKey: this.config.apiKey
-            });
-        }.bind(this), this.config.updateInterval);
-    },
-
-    // Handle the video titles received from the node helper.
-    socketNotificationReceived: function(notification, payload) {
-        if (notification === "VIDEO_TITLES") {
-            this.videoTitles = payload.titles.slice(0, this.config.maxTitles);
-            this.updateDom(this.config.animationSpeed);
-        }
+      this.updateDom();
     }
+  },
+
+  // Override dom generator.
+  getDom: function() {
+    var wrapper = document.createElement("div");
+    wrapper.innerHTML = html;
+    return wrapper;
+  }
 });
