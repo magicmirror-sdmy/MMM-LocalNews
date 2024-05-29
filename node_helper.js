1fc078dadca8c5e4e3fba865783d4dacc3b40eeb
sdmydbr9 <65818001+sdmydbr9@users.noreply.github.com>
2024-05-29 13:36:19 +0530

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,21 +1,18 @@
-var NodeHelper = require("node_helper");
+ var NodeHelper = require("node_helper");
 var https = require("https");
 
 module.exports = NodeHelper.create({
-  // Store the API key and channel IDs in local variables for faster access.
   apiKey: null,
   channelIds: null,
   updateInterval: null,
   debug: false,
   intervalId: null,
+  cache: {},
 
-  // Override start method.
   start: function() {
     console.log("Starting node helper for: " + this.name);
-    this.getData();
   },
 
-  // Handle the GET_VIDEO_TITLES socket notification.
   socketNotificationReceived: function(notification, payload) {
     if (notification === "GET_VIDEO_TITLES") {
       clearInterval(this.intervalId);
@@ -27,7 +24,6 @@ module.exports = NodeHelper.create({
     }
   },
 
-  // Fetch the video titles and send them back to the module.
   getData: function() {
     if (!this.channelIds) {
       return;
@@ -35,6 +31,16 @@ module.exports = NodeHelper.create({
 
     var self = this;
     this.channelIds.forEach(function(channelId) {
+      var cacheKey = `channel_${channelId}`;
+      var cachedData = self.cache[cacheKey];
+
+      // Check if the data is in cache and still valid
+      if (cachedData && (Date.now() - cachedData.timestamp < self.updateInterval)) {
+        console.log(`Using cached data for channel: ${channelId}`);
+        self.sendSocketNotification("VIDEO_TITLES", cachedData.data);
+        return;
+      }
+
       var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&maxResults=50&order=date&type=video&key=${self.apiKey}`;
 
       https.get(url, function(res) {
@@ -45,28 +51,58 @@ module.exports = NodeHelper.create({
         });
 
         res.on("end", function() {
-          var response = JSON.parse(body);
-          var titles = [];
+          try {
+            var response = JSON.parse(body);
+            if (response.error) {
+              console.error('Error response from YouTube API:', response);
+              if (response.error.code === 403) {
+                console.error('Quota exceeded. Stopping further requests.');
+                clearInterval(self.intervalId);
+              }
+              return;
+            }
 
-          for (var i = 0; i < response.items.length; i++) {
-            titles.push(response.items[i].snippet.title);
+            var videos = [];
+            if (response.items && Array.isArray(response.items)) {
+              for (var i = 0; i < response.items.length; i++) {
+                videos.push({
+                  title: response.items[i].snippet.title,
+                  channelTitle: response.items[i].snippet.channelTitle
+                });
+              }
+            } else {
+              console.error('Unexpected response format:', response);
+            }
+
+            if (self.debug) {
+              console.log("Videos: ", videos);
+            }
+
+            var payload = {
+              videos: videos,
+              channelId: channelId
+            };
+
+            // Cache the data
+            self.cache[cacheKey] = {
+              timestamp: Date.now(),
+              data: payload
+            };
+
+            self.sendSocketNotification("VIDEO_TITLES", payload);
+          } catch (error) {
+            console.error("Error processing response:", error);
           }
-
-          // Log the titles to the console if debug is enabled.
-          if (self.debug) {
-            console.log("Titles: " + titles);
-          }
-
-          // Send the video titles back to the module.
-          self.sendSocketNotification("VIDEO_TITLES", {
-            titles: titles,
-            channelId: channelId
-          });
         });
+
+        res.on("error", function(e) {
+          console.error("HTTP request error:", e);
+        });
+      }).on("error", function(e) {
+        console.error("HTTPS request error:", e);
       });
     });
 
-    // Set a timeout to fetch the data again after the update interval has passed.
     this.intervalId = setTimeout(function() {
       self.getData();
     }, this.updateInterval);
